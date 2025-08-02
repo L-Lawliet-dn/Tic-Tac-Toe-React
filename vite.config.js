5d665604b09d3282a0b61ef92b1bbdf371d7723f
Mohammad Soleh <soleh.mohammad198@gmail.com>
2025-08-02 19:01:28 +0530

    changed base so that could be deployed byy react

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'Tic-Tac-Toe-React', // 👈 VERY IMPORTANT
   plugins: [react()],
 })
