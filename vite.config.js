7b33756b0d03f7d82b6ffc47fb321ef13735112a
Mohammad Soleh <soleh.mohammad198@gmail.com>
2025-08-02 19:41:28 +0530

    added predeploy and deploy

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/Tic-Tac-Toe-React/', // 👈 VERY IMPORTANT
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/Tic-Tac-Toe-React' : '/',
   plugins: [react()],
-})
+}))
